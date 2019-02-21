4bbe43bee73643db86b2c2a539f9c366560f7b98
kapil-varshney <kvarshney.14@gmail.com>
2019-02-21 21:00:55 +0530

    Updated scrape_marathon_images.py

--- scrape_marathon_images.py
+++ scrape_marathon_images.py
@@ -1,10 +1,13 @@
+'''
+Usage: python scrape_marathon_images --fname <filepath of the img_pg_links CSV>
+'''
+
 # import the necessary packages
 from bs4 import BeautifulSoup as bs
 import requests
 import urllib.request
 import os
 import argparse
-#import config
 
 #Define the argument parser to read in the URL
 parser = argparse.ArgumentParser()
@@ -13,8 +16,8 @@ args = vars(parser.parse_args())
 f_name = args['fname']
 
 # Create the directory name where the images will be saved
-dir_name = f_name.split('.')[0]
-base_dir = '/home/kapil/bib-identification/dataset'
+base_dir = os.getcwd()
+dir_name = (f_name.split('/')[-1]).split('.')[0]
 dir_path = os.path.join(base_dir, dir_name)
 
 #Create the directory if already not there
@@ -28,6 +31,7 @@ links = f.read().split(',')
 # Print the number of images
 print ("[INFO] Downloading {} images".format(len(links)))
 
+# Function to take an image url and save the image in the given directory
 def download_image(url):
     print("[INFO] downloading {}".format(url))
     name = str(url.split('/')[-1])
